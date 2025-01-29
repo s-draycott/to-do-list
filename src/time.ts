8263271f57c00b87fd6533e38469e856fef74cb0
unknown <sally-draycott@hotmail.co.uk>
2025-01-29 16:36:50 +0000

    setup for deployment

--- src/time.ts
+++ src/time.ts
@@ -1,29 +1,29 @@
-// const timeOfDay = document.querySelector<HTMLSpanElement>("#timeOfDay");
-// const imgContainer = document.querySelector<HTMLSpanElement>("#imgContainer")
+const timeOfDay = document.querySelector<HTMLSpanElement>("#timeOfDay");
+const imgContainer = document.querySelector<HTMLSpanElement>("#imgContainer")
 
-// if (!timeOfDay ||!imgContainer) {
-//       throw new Error("Issues with the HTM selectors")
-//     }
+if (!timeOfDay ||!imgContainer) {
+      throw new Error("Issues with the HTM selectors")
+    }
 
-// const currentHour = new Date().getHours()
-// console.log(currentHour)
-// if (currentHour < 12) {
-//   timeOfDay.innerText = "Morning"
-//   let icon = document.createElement("img")
-//   icon.src = "src/content/sunrise-icon.png"
-//   icon.className = "headingContainer headingContainer__icon"
-//   imgContainer.appendChild(icon)
-// } else if (currentHour < 18) {
-//   timeOfDay.innerText = "Afternoon"
-//   let icon = document.createElement("img")
-//   icon.src = "src/content/sun-icon.png"
-//   icon.className = "headingContainer headingContainer__icon"
-//   imgContainer.appendChild(icon)
-// } else {
-//   timeOfDay.innerText = "Evening"
-//   let icon = document.createElement("img")
-//   icon.src = "src/content/moon-icon-2048x2047-h5hsykin.png"
-//   icon.className = "headingContainer headingContainer__icon"
-//   imgContainer.appendChild(icon)
+const currentHour = new Date().getHours()
+console.log(currentHour)
+if (currentHour < 12) {
+  timeOfDay.innerText = "Morning"
+  let icon = document.createElement("img")
+  icon.src = "src/content/sunrise-icon.png"
+  icon.className = "headingContainer headingContainer__icon"
+  imgContainer.appendChild(icon)
+} else if (currentHour < 18) {
+  timeOfDay.innerText = "Afternoon"
+  let icon = document.createElement("img")
+  icon.src = "src/content/sun-icon.png"
+  icon.className = "headingContainer headingContainer__icon"
+  imgContainer.appendChild(icon)
+} else {
+  timeOfDay.innerText = "Evening"
+  let icon = document.createElement("img")
+  icon.src = "src/content/moon-icon-2048x2047-h5hsykin.png"
+  icon.className = "headingContainer headingContainer__icon"
+  imgContainer.appendChild(icon)
 
-// }
+}
