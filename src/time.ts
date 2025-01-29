2c57f86d329ca63a08fde603549f9bd492763ea8
unknown <sally-draycott@hotmail.co.uk>
2025-01-29 16:28:12 +0000

    setup for deployment

--- src/time.ts
+++ src/time.ts
@@ -10,19 +10,19 @@ console.log(currentHour)
 if (currentHour < 12) {
   timeOfDay.innerText = "Morning"
   let icon = document.createElement("img")
-  icon.src = "/src/content/sunrise-icon.png"
+  icon.src = "src/content/sunrise-icon.png"
   icon.className = "headingContainer headingContainer__icon"
   imgContainer.appendChild(icon)
 } else if (currentHour < 18) {
   timeOfDay.innerText = "Afternoon"
   let icon = document.createElement("img")
-  icon.src = "./src/content/sun-icon.png"
+  icon.src = "src/content/sun-icon.png"
   icon.className = "headingContainer headingContainer__icon"
   imgContainer.appendChild(icon)
 } else {
   timeOfDay.innerText = "Evening"
   let icon = document.createElement("img")
-  icon.src = "/src/content/moon-icon-2048x2047-h5hsykin.png"
+  icon.src = "src/content/moon-icon-2048x2047-h5hsykin.png"
   icon.className = "headingContainer headingContainer__icon"
   imgContainer.appendChild(icon)
 
