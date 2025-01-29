aa5370b2c6fc1d7aea9389a490e50e7cb9033ce3
unknown <sally-draycott@hotmail.co.uk>
2025-01-29 11:32:04 +0000

    added time of day function

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,20 @@
 import './style.css'
 import './api'
+
+const timeOfDay = document.querySelector<HTMLSpanElement>("#timeOfDay");
+
+if (!timeOfDay) {
+      throw new Error("Issues with the HTM selectors")
+    }
+
+const currentHour = new Date().getHours()
+console.log(currentHour)
+
+if (currentHour < 12) {
+  timeOfDay.innerText = "morning"
+  console.log("morning")
+} else if (currentHour < 18) {
+  console.log("afternoon")
+} else {
+  console.log("evening")
+}
