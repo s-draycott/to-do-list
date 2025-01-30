10106805ab81c4c85dd3334cd555d5e5daba03f2
unknown <sally-draycott@hotmail.co.uk>
2025-01-30 12:45:06 +0000

    style update

--- src/time.ts
+++ src/time.ts
@@ -9,11 +9,11 @@ const currentHour = new Date().getHours()
 
 if (currentHour < 12) {
   timeOfDay.innerText = "Morning"
-  timeImg.src = "/to-do-list/src/assets/sunrise-icon.png";
+  timeImg.src = "to-do-list/public/sunrise-icon.png";
 } else if (currentHour < 18) {
   timeOfDay.innerText = "Afternoon"
-  timeImg.src = "/to-do-list/src/assets/sun-icon.png";
+  timeImg.src = "to-do-list/public/sun-icon.png";
 } else {
   timeOfDay.innerText = "Evening";
-  timeImg.src = "/to-do-list/src/assets/moon-icon.png";
+  timeImg.src = "to-do-list/public/moon-icon.png";
 }
